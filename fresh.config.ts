fc2f2b7fbf44a29b0cf79529b880867252618544
Bunyasit Fang <wachaa1319@gmail.com>
2023-12-07 23:41:04 +0700

    Update environment variables and workflow configuration

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,15 +1,5 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-import { z } from "zod";
-console.log("yoyo");
-const envParser = z.object({
-  ENV: z.string(),
-  // GOOGLE_CLIENT_ID: z.string(),
-  // GOOGLE_CLIENT_SECRET: z.string(),
-  APP_KEY: z.string(),
-});
-
-export const globalEnv = envParser.parse(Deno.env.toObject());
 
 export default defineConfig({
   plugins: [tailwind()],
